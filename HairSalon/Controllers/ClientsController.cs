a030a75203172630b8d7064b325c9259d98148b4
Ashley Porter <porter.ashley13@gmail.com>
2021-03-13 09:12:04 -0800

    update file to prep for database connection

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,21 +1,72 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
-  public class ClientsController : Controller
+  public class StylistsController : Controller
   {
+    private readonly HairSalonContext _db;
 
-    [HttpGet("/stylists/{stylistId}/clients/new")]
-    public ActionResult New(int stylistId)
+    public StylistsController(HairSalonContext db)
     {
+      _db = db;
     }
 
-    [HttpGet("/stylists/{stylistId}/clients/{clientId}")]
-    public ActionResult Show(int stylistId, int clientId)
+    public ActionResult Index()
     {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
     }
 
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist category)
+    {
+      _db.Stylists.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      ViewBag.thisStylist = _db.Stylists.FirstOrDefault(cuisine => cuisine.StylistId == id);
+      ViewBag.clients = _db.Clients.Where(client => client.StylistId == id).ToList();
+      return View();
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(category => category.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist category)
+    {
+      _db.Entry(category).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(category => category.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(category => category.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
