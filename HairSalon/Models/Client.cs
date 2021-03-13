620b3a96e8f721f52a549071db3cd384cff60072
Ashley Porter <porter.ashley13@gmail.com>
2021-03-13 09:31:33 -0800

    update variables to match database column names

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,9 +3,9 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
-    public string Name { get; set; }
-    public string Address { get; set; }
-    public long PhoneNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string ContactInfo { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
